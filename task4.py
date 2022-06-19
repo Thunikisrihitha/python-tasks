431726f9b423e49342f683de21eae590d0bcc4e8
Thunikisrihitha <107308955+Thunikisrihitha@users.noreply.github.com>
2022-06-19 17:10:10 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,15 @@
+#comments
+#single line comments  #is used
+'''multiple comments 
+triple quotations are used'''
+
+a=252
+b=5.55
+c=2+6j
+d=float(a)
+e=int(a)
+print(type(a))
+print(type(b))
+print(type(c))
+print(d)
+print(e)
