15922ec77b6ca8984dc484a3b4c4b61c6704c323
Thunikisrihitha <107308955+Thunikisrihitha@users.noreply.github.com>
2022-06-19 17:01:21 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,12 @@
+#Simple interest and compound interest
+#simple interest=PTR/100      compoud interest=p(1+r/100)n
+P=10000
+T=2
+R=3
+N=2
+simple_interest=(P*T*R)/100
+print("the simple interest:",simple_interest)
+
+
+compound_interest=P*(1+R/100)*N
+print("the compound interest:",compound_interest)
